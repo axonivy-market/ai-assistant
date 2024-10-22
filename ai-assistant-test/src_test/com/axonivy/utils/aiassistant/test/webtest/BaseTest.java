0e3ff7e88cabd1c0d2938b4f396cdcec40bd8749
Hoan Nguyen <83745591+nqhoan-axonivy@users.noreply.github.com>
2024-10-22 10:58:05 +0700

    Fix tests (#2)
    IVYPORTAL-17131 Stage 1: Create a Marketplace item for Portal Assistant
    - Fixed and clean tests
    
    ---------
    
    Co-authored-by: mnhnam-axonivy <nam.mai@axonactive.com>
    Co-authored-by: Tung Le <tung.le@axonactive.com>

--- ai-assistant-test/src_test/com/axonivy/utils/aiassistant/test/webtest/BaseTest.java
+++ ai-assistant-test/src_test/com/axonivy/utils/aiassistant/test/webtest/BaseTest.java
@@ -3,6 +3,7 @@ package com.axonivy.utils.aiassistant.test.webtest;
 import static com.codeborne.selenide.Selenide.open;
 
 import com.axonivy.ivy.webtest.engine.EngineUrl;
+import com.axonivy.ivy.webtest.engine.WebAppFixture;
 import com.axonivy.utils.aiassistant.test.page.AssistantManagementPage;
 
 public abstract class BaseTest {
@@ -10,5 +11,9 @@ public abstract class BaseTest {
   protected AssistantManagementPage navigateToAiManagementPage() {
     open(EngineUrl.createProcessUrl("/ai-assistant/19198CAD6D9F2F9E/AiManagement.ivp"));
     return new AssistantManagementPage();
+  }
+
+  protected void loginAsDeveloper(WebAppFixture fixture) {
+    fixture.login("Developer", "Developer");
   }
 }
